e25ac81717cff5f8a515b45eb40d86b3ac258c05
Kamali-696 <kamalikamuruganandham@gmail.com>
2026-06-06 12:18:15 +0530

    feat: added hyperparameter tuning and cross validation for best model

--- src/train.py
+++ src/train.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from sklearn.model_selection import train_test_split,StratifiedKFold
+from sklearn.model_selection import train_test_split,StratifiedKFold,cross_val_score,GridSearchCV
 from sklearn.preprocessing import RobustScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
@@ -54,3 +54,67 @@ def svm(X_train,y_train):
     pipeline_svm.fit(X_train,y_train)
     print("SVC model trained!!")
     return pipeline_svm
+
+
+#Best models: SVC and Logistic Regression
+
+# Performing Hyperparameter tuning and Stratified K-Fold cross validation for exploring better results
+def logistic_gridSearchCV(X_train,y_train):
+
+    pipeline = Pipeline([
+        ('scaler',RobustScaler()),
+        ('model',LogisticRegression(class_weight='balanced',max_iter=1000))
+    ])
+
+    param_grid = {
+        "model__C": [0.01,0.1,1,10,100]
+    }
+
+    skf = StratifiedKFold(
+        n_splits=5,
+        shuffle=True,
+        random_state=42
+    )
+
+    grid_logistic = GridSearchCV(
+        pipeline,
+        param_grid,
+        cv=skf,
+        scoring="roc_auc",
+        n_jobs= -1
+    )
+
+    grid_logistic.fit(X_train,y_train)
+
+    return grid_logistic
+
+
+def svm_gridSearchCV(X_train,y_train):
+
+    pipeline = Pipeline([
+        ('scaler',RobustScaler()),
+        ('model',SVC(class_weight='balanced',random_state=42))
+    ])
+
+    param_grid = {
+        "model__C": [0.01,0.1,1,10,100],
+        "model__kernel": ['rbf','linear']
+    }
+
+    skf = StratifiedKFold(
+        n_splits=5,
+        shuffle=True,
+        random_state=42
+    )
+
+    grid_svm = GridSearchCV(
+        pipeline,
+        param_grid,
+        cv=skf,
+        scoring="roc_auc",
+        n_jobs= -1
+    )
+
+    grid_svm.fit(X_train,y_train)
+
+    return grid_svm
