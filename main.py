413246854a9ae2461e6252cf702c253d5f9f8fce
Kamali-696 <kamalikamuruganandham@gmail.com>
2026-06-07 18:30:36 +0530

    refactor: added comments for better readability

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from src.train import split_and_load_data,logistic_regression,decision_tree,random_forest,svm,logistic_gridSearchCV,svm_gridSearchCV
+from src.train import split_and_load_data,logistic_gridSearchCV
 from src.evaluate import evaluate_model
+from src.utils import save_model
 import pandas as pd
-import joblib
 
 X_train,X_test,y_train,y_test = split_and_load_data(r"D:\ML_LEARNING\PROJECTS\customer-churn-prediction\data\preprocessed_data\churn_processed.csv")
 
@@ -32,6 +32,13 @@ X_train,X_test,y_train,y_test = split_and_load_data(r"D:\ML_LEARNING\PROJECTS\cu
 # sv = svm(X_train,y_train)
 # evaluate_model(sv,X_test,y_test)
 
+# print("===== SUPPORT VECTOR CLASSIFIER - TUNED =====")
+# svc_tuned = svm_gridSearchCV(X_train,y_train)
+# print(svc_tuned.best_params_)
+# print(svc_tuned.best_score_)
+
+# FINALISED MODEL: TUNED LOGISTIC REGRESSION
+
 print("====== LOGISTIC REGRESSION - TUNED ======")
 lr_tuned = logistic_gridSearchCV(X_train,y_train)
 print(lr_tuned.best_params_)
@@ -47,14 +54,33 @@ evaluate_model(
     X_test,
     y_test
 )
-# print("===== SUPPORT VECTOR CLASSIFIER - TUNED =====")
-# svc_tuned = svm_gridSearchCV(X_train,y_train)
-# print(svc_tuned.best_params_)
-# print(svc_tuned.best_score_)
 
-joblib.dump(
-    best_lr,
-    "models/churn_model.pkl"
+#save the model as .pkl file
+
+save_model(best_lr,"models/churn_model.pkl")
+
+# Checking the feature importance of the features for model interpretation and EDA comparision
+
+lr_model = best_lr.named_steps["model"]
+
+coef = lr_model.coef_[0]
+
+importance_df = pd.DataFrame({
+    "Feature": X_train.columns,
+    "Coefficient": coef
+})
+
+importance_df = importance_df.sort_values(
+    by="Coefficient",
+    ascending=False
 )
 
-print("Model saved successfully!")
+# higher the values - more likely to be churned
+print(importance_df.head(10))
+
+print()
+print()
+
+# higher the values - less likely to be churned
+print(importance_df.tail(10))
+
